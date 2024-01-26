b0a315efa60d2dadee464b8fdcc77df61ed61c3a
mal0101 <malakmekyassi04@gmail.com>
2024-01-26 05:54:58 -0800

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""square"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size=0):
+        """initializing class
+        size - size of square
+        raises: TypeError, size not int
+        ValueError, size < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
